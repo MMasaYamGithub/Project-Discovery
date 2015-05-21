81ea4fccfae45731d8e594325d044d6148dcd929
Matster2 <matster134@ntlworld.com>
2015-05-21 20:09:55 +0100

    Earth & Moon Prototype Rotation & Orbit

---
+++ Discovery/Assets/MoonTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonTest : MonoBehaviour {
+	
+	public Transform target;
+	public float RotationSpeed = 100f;
+	public float OrbitDegrees = 1f;
+
+	void Update () {
+		transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+		transform.position = RotatePointAroundPivot(transform.position, transform.parent.position, Quaternion.Euler(0, OrbitDegrees * Time.deltaTime, 0));
+	}
+
+	public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle) {
+		return angle * ( point - pivot) + pivot;
+	}
+}
