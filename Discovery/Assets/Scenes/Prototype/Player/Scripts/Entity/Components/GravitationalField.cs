c67c326e90c6355901c4dcbd3954232c3b5d1298
ALPSquid <alpsquid@kolabnow.com>
2015-07-08 19:01:23 +0100

    WIP: Realistic gravity calculation based on mass and radius of object in relation to Earth. Gravity scaling based on distance from object surface

--- Discovery/Assets/Scenes/Prototype/Player/Scripts/Entity/Components/GravitationalField.cs
+++ Discovery/Assets/Scenes/Prototype/Player/Scripts/Entity/Components/GravitationalField.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using DefinitiveStudios.Discovery.Core.Utils;
@@ -8,63 +9,78 @@ namespace DefinitiveStudios.Discovery.Prototype.Player.Entity.Components {
 
     /// <summary>
     /// Component for simulating gravitational fields. The collider is the max gravitational field range and will be set to be a trigger.
-    /// Works with most 3D objects.
-    /// TODO: Use simple normal calculation for spheres (transform.position - target.transform.position)
-    /// TODO: Multiply gravity based on distance from origin
+    /// Works with most (all?) 3D objects.
     /// </summary>
-    [RequireComponent(typeof(Mesh))]
+    //[RequireComponent(typeof(Mesh))]
     public class GravitationalField : MonoBehaviour {
 
-        public float gravity = 9.8f;
+        private const float EARTH_MASS = 5.976f; // * 10^24
+        private const float EARTH_RADIUS = 6371f;
+        private const float EARTH_SURFACE_GRAVITY = 9.8f;
+
+        public float surfaceGravity = 9.8f;
+        public float massMultiple = 1;  // Mass in relation to Earth
+        public float radiusMultiple = 1;  // Radius in relation to Earth
+        public bool useRelationalValues = true;  // Whether to calculate surface gravity and max gravitational distance based on the above relational values.
+                                                 // If false, the collider radius will be used as the max field distance and gravity will not be changed.
+                                                 // If true, gravity and max field distance will be calculated based on the relational value. 
+        private float gravityThreshold = 0.5f;  // Falloff for max gravity distance. The gravitational field will stop when gravity is smaller than this value.
+                                               // Only used when useRelationalValue sis enabled
         public Collider fieldCollider;
         private readonly List<Rigidbody> contactBodies = new List<Rigidbody>();
+        private float radius;
+        private float mass;
         // optimisation vars
-        private Vector3 currPosition;
-        private Vector3 prevPosition;
-        private Quaternion currRotation;
-        private Quaternion prevRotation;
         private Vector3 gravVector;
-        private Mesh mesh;
-        private bool isSphere;
-        private bool flipNormal;
         private RaycastHit hit;
 
 
-        private void Start() {
-            mesh = GetComponent<MeshFilter>().mesh;
+        protected void Start() {
             if (fieldCollider == null) throw new UnityException("Gravitational Field requires a field collider!");
             fieldCollider.isTrigger = true;
-            isSphere = fieldCollider is SphereCollider;
+
+            mass = EARTH_MASS * massMultiple;
+            radius = EARTH_RADIUS * radiusMultiple;
+            if (useRelationalValues) {
+                surfaceGravity = ((6.674f * mass) / (radius * radius)) * 10; // TODO: Fix calculation error, numbers are too small and messing up distance calculations
+                // Set max field distance to something realistic
+                // TODO: This might actually affect gameplay in a negative way (things apparently moving on their own due to a distant planet)
+                //float gravityThresholdMultiplier = (gravityThreshold * gravityThreshold) * 10000; // Distance required to reach a gravitational force of 0.5
+                //if (fieldCollider is SphereCollider) {
+                //    ((SphereCollider)fieldCollider).radius = gravityThresholdMultiplier;
+                //} else if (fieldCollider is CapsuleCollider) {
+                //    ((CapsuleCollider)fieldCollider).radius = gravityThresholdMultiplier;
+                //    ((CapsuleCollider)fieldCollider).height = gravityThresholdMultiplier;
+                //}else if (fieldCollider is BoxCollider) {
+                //    ((BoxCollider)fieldCollider).size = new Vector3(gravityThresholdMultiplier, gravityThresholdMultiplier, gravityThresholdMultiplier);
+                //}
+            }
         }
 
-        private void FixedUpdate() {
-            if (!isSphere) {
-                prevPosition = currPosition;
-                prevRotation = currRotation;
-                currPosition = transform.position;
-                currRotation = transform.rotation;
-                if (!currPosition.Equals(prevPosition) || !currRotation.Equals(prevRotation)) gravVector = Utils.GetNormal(mesh);
-            }
+        protected void FixedUpdate() {
             foreach (Rigidbody body in contactBodies) {
-                //if (isSphere) gravVector = (transform.position - body.transform.position).normalized;
-                //else {
-                //    // Reverse the normal if the target is below the mesh
-                //    flipNormal = (Vector3.Dot(Vector3.up, body.transform.position - transform.position) < 0);
-                //}
-                //body.AddForce(((flipNormal)? gravVector : -gravVector) * gravity);
+                // TODO: Layer index manager (layer 8)
                 if (Physics.Raycast(body.position, transform.position - body.position, out hit, Vector3.Distance(transform.position, body.position), 1 << 8)) {
                     Debug.DrawRay(body.position, transform.position - body.position);
-                    body.rotation = Quaternion.Slerp(body.rotation, Quaternion.FromToRotation(Vector3.up, hit.normal), Time.fixedDeltaTime * 5);
-                    body.AddForce(-hit.normal * gravity); // TODO: SCale gravity based on distance from center
+                    // TODO: Don't interpolate local y
+                    Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                    targetRotation.y = body.rotation.y;
+                    body.rotation = Quaternion.Slerp(body.rotation, targetRotation, Time.fixedDeltaTime * 5);
+                    body.AddForce(-hit.normal * GetGravityAt(hit.distance));
+                    if (fieldCollider is SphereCollider) print(GetGravityAt(hit.distance));
                 }
             }
         }
 
-        private void OnTriggerEnter(Collider other) {
+        protected float GetGravityAt(float distance) {
+            return (surfaceGravity * Mathf.Pow(radius / (radius + distance), 2));
+        }
+
+        protected void OnTriggerEnter(Collider other) {
             contactBodies.Add(other.attachedRigidbody);
         }
 
-        private void OnTriggerExit(Collider other) {
+        protected void OnTriggerExit(Collider other) {
             contactBodies.Remove(other.attachedRigidbody);
         }
     }
